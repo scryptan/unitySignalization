68a44c8f62e132201e8b8cdf0905c97c1a78e796
Maxim Tretyakov <scryptan@gmail.com>
2021-02-01 17:36:08 +0500

    rename function to more abstract

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,7 +5,8 @@ using Random = UnityEngine.Random;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject _enemyPrefab;
-
+    
+    private float _spawnRateInSeconds = 2;
     private Transform[] _spawnPoints;
     
     private void Start()
@@ -15,16 +16,16 @@ public class EnemySpawner : MonoBehaviour
         for (int i = 0; i < transform.childCount; i++)
             _spawnPoints[i] = transform.GetChild(i);
 
-        StartCoroutine(SpawnEnemyInTwoSeconds());
+        StartCoroutine(SpawnEnemyInSeconds(_spawnRateInSeconds));
     }
 
-    private IEnumerator SpawnEnemyInTwoSeconds()
+    private IEnumerator SpawnEnemyInSeconds(float spawnRateInSeconds)
     {
         while (true)
         {
             var randomPoint = Random.Range(0, _spawnPoints.Length);
             Instantiate(_enemyPrefab, _spawnPoints[randomPoint].position, Quaternion.identity);
-            yield return new WaitForSeconds(2);
+            yield return new WaitForSeconds(spawnRateInSeconds);
         }
     }
 }
